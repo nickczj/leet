c00e1991e7517b89d2b265cdcdb166ea7149cd73
fion.tan <fion.tan@archerlogic.com>
2021-06-19 16:47:29 +0800

    🐈🐈🐈🐈

---
+++ cracking/isUniqueFion.ts
@@ -0,0 +1,29 @@
+const anExampleVariable:string = "Hello World"
+console.log(anExampleVariable)
+
+let holder2:string[] = [];
+
+let isUnique = (s: string): boolean => {
+    
+    interface Holder {
+        [details: string]: string;
+    }
+    let holder: Holder = {};
+    let strlen:number = anExampleVariable.length;
+
+    for(var i = 0; i < strlen; i++)
+    {
+        let tmp:string = anExampleVariable[i];
+        if (typeof holder[tmp] != 'undefined')
+        {
+            return false;
+        }
+        holder[tmp] = tmp;
+    }
+    return true;
+}
+
+console.log(isUnique(anExampleVariable));
+
+let v:number = 'a'.charCodeAt(0) - 'a'.charCodeAt(0);
+console.log(v);
